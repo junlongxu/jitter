e392c71cd5c47b09cd6cf92a365f852482b82588
kinko <87232214@qq.com>
2020-08-18 22:40:09 +0800

    完成视频播放滑动列表

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,12 +7,106 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with Base {
+class _HomePageState extends State<HomePage>
+    with Base, SingleTickerProviderStateMixin {
+  final List<String> tabList = ['关注', '推荐'];
+  TabController _tabController;
+  @override
+  void initState() {
+    _tabController = new TabController(length: tabList.length, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xff000000),
-      child: Player(url: 'https://media.w3.org/2010/05/sintel/trailer.mp4',),
+      color: Colors.black,
+      // child: Player(
+      //   url: 'http://www.w3school.com.cn/example/html5/mov_bbb.mp4',
+      // ),
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: _tabPageView(),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 20,
+              child: _tabBarLabel(),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+// 顶部导航
+  _tabBarLabel() {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: TabBar(
+        controller: _tabController,
+        indicatorColor: Colors.white,
+        indicatorWeight: 3.0,
+        isScrollable: true,
+        indicatorSize: TabBarIndicatorSize.label,
+        tabs: <Widget>[
+          ...tabList.map<Tab>((tab) => Tab(
+                child: Text(
+                  tab,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+
+// 页面
+  _tabPageView() {
+    return TabBarView(
+        controller: _tabController,
+        children: tabList.map<Widget>((value) => _tabView(value)).toList());
+  }
+
+  _tabBar() {
+    return Text('data');
+  }
+
+  _tabView(String name) {
+    List<String> list = [
+      'http://www.w3school.com.cn/example/html5/mov_bbb.mp4',
+      'http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4',
+      'http://vfx.mtime.cn/Video/2019/03/19/mp4/190319222227698228.mp4'
+    ];
+    return name != '推荐'
+        ? Icon(
+            Icons.ac_unit,
+            color: Colors.white,
+          )
+        : PageView.builder(
+            itemCount: list?.length ?? 0,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (BuildContext context, int index) {
+              return Player(url: list[index]);
+            });
+  }
+  // @override
+  // bool get wantKeepAlive => true;
 }
--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-// import 'package:getflutter/getflutter.dart';
+import 'package:loading/loading.dart';
+import 'package:loading/indicator/ball_spin_fade_loader_indicator.dart';
 
 class Player extends StatefulWidget {
   final String url;
-  const Player({this.url});
+  const Player({@required this.url});
   @override
   _PlayerState createState() => _PlayerState();
 }
 
 class _PlayerState extends State<Player> {
   VideoPlayerController _controller;
+  bool isPause = false;
   @override
   void initState() {
     _controller = VideoPlayerController.network(widget?.url)
@@ -41,16 +43,47 @@ class _PlayerState extends State<Player> {
           ? GestureDetector(
               onTap: () {
                 if (_controller.value.isPlaying) {
+                  isPause = true;
                   _controller.pause();
                 } else {
+                  isPause = false;
                   _controller.play();
                 }
               },
               child: AspectRatio(
                 aspectRatio: 0.66,
                 child: VideoPlayer(_controller),
-              ))
-          : Text('loading'),
+              ),
+              // child: Center(
+              //   child: Stack(
+              //     children: <Widget>[
+              //       AspectRatio(
+              //         aspectRatio: 0.66,
+              //         child: VideoPlayer(_controller),
+              //       ),
+              //       isPause
+              //           ? Positioned(
+              //               top: MediaQuery.of(context).size.height * 0.4,
+              //               left: MediaQuery.of(context).size.width * 0.4,
+              //               child: Text('11111'))
+              //           : Positioned(
+              //               top: MediaQuery.of(context).size.height * 0.4,
+              //               left: MediaQuery.of(context).size.width * 0.4,
+              //               child: Image.asset(
+              //                 'assets/images/pause.png',
+              //                 width: 80,
+              //                 height: 80,
+              //               ))
+              //     ],
+              //   ),
+              // )
+            //   left: 0,
+            // right: 0,
+            // bottom: 0,
+            // top: 54,
+ 
+            )
+          : Loading(indicator: BallSpinFadeLoaderIndicator(), size: 30.0),
     );
   }
 }
