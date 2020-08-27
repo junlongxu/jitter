4e4dc688166691405b497c1bafb5c805e2f01a9a
kinko <87232214@qq.com>
2020-08-27 16:21:33 +0800

    完成h5嵌套

--- lib/pages/taskpage/index.dart
+++ lib/pages/taskpage/index.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jitter/util/base.dart';
 import 'dart:async';
-
 import 'package:jitter/widgets/web_view_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,7 +9,8 @@ class TaskPage extends StatefulWidget {
   _TaskPageState createState() => _TaskPageState();
 }
 
-class _TaskPageState extends State<TaskPage> with Base {
+class _TaskPageState extends State<TaskPage>
+    with Base, AutomaticKeepAliveClientMixin {
   @override
   void initState() {
     // TODO: implement initState
@@ -31,37 +31,27 @@ class _TaskPageState extends State<TaskPage> with Base {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        flex: 1,
-        child: FutureBuilder<String>(
-          future: getToken(),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text('loading');
-            } else if (snapshot.data != null) {
-              return WebViewWidget(
-                url:
-                    'http://www.11wy.top/taskpage/#/task?token=${snapshot.data}',
-                // url: 'http://172.24.135.208/taskpage/#/task?token=$token',
-                statusBarColor: '242a37',
-                title: 'H5',
-                hideAppBar: true,
-                bottomAppBar: false,
-              );
-            }
-          },
-        ));
+    return Container(
+      height: 200,
+      child: Expanded(
+          flex: 1,
+          child: FutureBuilder<String>(
+            future: getToken(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Text('loading');
+              } else if (snapshot.data != null) {
+                return WebViewWidget(
+                  url:
+                      'http://www.11wy.top/taskpage/#/task?token=${snapshot.data}',
+                );
+              }
+            },
+          )),
+    );
   }
 
-  // Expanded(
-  //     child: WebViewWidget(
-  //         // 5450e83c1a2f4d79f5c05330c258c520
-  //         url: 'http://www.11wy.top/taskpage/#/task?token=$token',
-  //         // url: 'http://172.24.135.208/taskpage/#/task?token=$token',
-  //         statusBarColor: '242a37',
-  //         title: 'H5',
-  //         hideAppBar: true,
-  //         bottomAppBar: false),
-  //   );
-
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => true;
 }
